c3e38dcf6af66169be3025886493fdbae6d54e6f
Ly <h_ly70268@tss-hr420-dm17.fc.ca>
2018-06-13 14:03:10 -0400

    Listener Added

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
 (function () {
 	console.log("Js File Loaded")
-}
-
-//Vars
 
 
+//Vars Variables
 
+var num1 = document.querySelector ("#input1");
+var num2 = document.querySelector ("#input2");
+var equals = document.querySelector ("#trigger");
+var outcome = document.querySelector ("#value");
+//console.log(num1);
+//console.log(num2);
+//console.log(equals);
+//console.log(outcome);
 
 //Functions
 	
-
+function addStuff() {
+	console.log("from addStuff")
+}
 
 
 //Listeners
-
+equals.addEventListener("click",addStuff,false)
 
 
 
